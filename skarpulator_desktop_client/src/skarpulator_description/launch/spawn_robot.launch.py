599d5823949aae9f274f2c4c94da3dae0ae9c6fe
Shubh Khandelwal <shubh4664@gmail.com>
2026-07-29 16:49:40 +0530

    Fixed colcon test errors

--- skarpulator_desktop_client/src/skarpulator_description/launch/spawn_robot.launch.py
+++ skarpulator_desktop_client/src/skarpulator_description/launch/spawn_robot.launch.py
@@ -5,78 +5,86 @@ from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
+
 def generate_launch_description():
 
-    world = LaunchConfiguration("world")
+    world = LaunchConfiguration('world')
 
     gz_node = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(PathJoinSubstitution([FindPackageShare("ros_gz_sim"), "launch", "gz_sim.launch.py"])),
-        launch_arguments = {
-            "gz_args" : ["-r -s -v4 ", world, " --physics-engine gz-physics-bullet-featherstone-plugin"]
+        PythonLaunchDescriptionSource(PathJoinSubstitution([
+            FindPackageShare('ros_gz_sim'),
+            'launch',
+            'gz_sim.launch.py'
+        ])),
+        launch_arguments={
+            'gz_args': [
+                '-r -s -v4 ', world,
+                ' --physics-engine gz-physics-bullet-featherstone-plugin'
+            ]
         }.items(),
     )
 
     rsp_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(PathJoinSubstitution([
-            FindPackageShare("skarpulator_description"),
-            "launch",
-            "rsp.launch.py"
+            FindPackageShare('skarpulator_description'),
+            'launch',
+            'rsp.launch.py'
         ])),
-        launch_arguments = {
-            "description_file" : PathJoinSubstitution([
-                FindPackageShare("skarpulator_description"),
-                "models",
-                "robot.urdf.xacro"
+        launch_arguments={
+            'description_file': PathJoinSubstitution([
+                FindPackageShare('skarpulator_description'),
+                'models',
+                'robot.urdf.xacro'
             ])
         }.items(),
     )
 
     ros_gz_bridge_node = Node(
-        package = "ros_gz_bridge",
-        executable = "parameter_bridge",
-        arguments = [
-            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
         ],
-        output = "screen",
+        output='screen',
     )
 
     entity_spawner_node = Node(
-        package = "ros_gz_sim",
-        executable = "create",
-        output = "screen",
-        arguments = [
-            "-name",
-            "ur",
-            "-topic",
-            "robot_description",
-            "-allow_renaming",
-            "true"
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=[
+            '-name',
+            'ur',
+            '-topic',
+            'robot_description',
+            '-allow_renaming',
+            'true'
         ],
     )
 
     joint_state_broadcaster_spawner_node = Node(
-        package = "controller_manager",
-        executable = "spawner",
-        arguments = ["joint_state_broadcaster", "-c", "/controller_manager"]
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster', '-c', '/controller_manager']
     )
 
     joint_controller_spawner_node = Node(
-        package = "controller_manager",
-        executable = "spawner",
-        arguments = ["scaled_joint_trajectory_controller", "-c", "/controller_manager"]
+        package='controller_manager',
+        executable='spawner',
+        arguments=['scaled_joint_trajectory_controller', '-c', '/controller_manager']
     )
 
     gripper_controller_spawner_node = Node(
-        package = "controller_manager",
-        executable = "spawner",
-        arguments = ["robotiq_gripper_controller", "-c", "/controller_manager"]
+        package='controller_manager',
+        executable='spawner',
+        arguments=['robotiq_gripper_controller', '-c', '/controller_manager']
     )
 
     return LaunchDescription([
         DeclareLaunchArgument(
-            "world",
-            default_value = "empty.sdf",
-            description = "Gazebo world file (absolute path or filename from the gz sim worlds collection) containing a custom world.",
+            'world',
+            default_value='empty.sdf',
+            description='Gazebo world file containing a custom world.',
         ),
         gz_node,
         rsp_node,
--- skarpulator_desktop_client/src/skarpulator_description/launch/view_robot.launch.py
+++ skarpulator_desktop_client/src/skarpulator_description/launch/view_robot.launch.py
@@ -5,44 +5,49 @@ from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
+
 def generate_launch_description():
-    
-    rviz_config = LaunchConfiguration("rviz_config")
+
+    rviz_config = LaunchConfiguration('rviz_config')
 
     rsp_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(PathJoinSubstitution([
-            FindPackageShare("skarpulator_description"),
-            "launch",
-            "rsp.launch.py"
+            FindPackageShare('skarpulator_description'),
+            'launch',
+            'rsp.launch.py'
         ])),
-        launch_arguments = {
-            "description_file" : PathJoinSubstitution([
-                FindPackageShare("skarpulator_description"),
-                "models",
-                "robot.urdf.xacro"
+        launch_arguments={
+            'description_file': PathJoinSubstitution([
+                FindPackageShare('skarpulator_description'),
+                'models',
+                'robot.urdf.xacro'
             ])
         }.items(),
     )
 
     rviz_node = Node(
-        package = "rviz2",
-        executable = "rviz2",
-        name = "rviz2",
-        output = "log",
-        arguments = ["-d", rviz_config]
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='log',
+        arguments=['-d', rviz_config]
     )
 
     joint_state_publisher_node = Node(
-        package = "joint_state_publisher",
-        executable = "joint_state_publisher",
-        name = "joint_state_publisher"
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher'
     )
 
     return LaunchDescription([
         DeclareLaunchArgument(
-            "rviz_config",
-            default_value = PathJoinSubstitution([FindPackageShare("skarpulator_description"), "rviz", "view.rviz"]),
-            description = "Rviz config file (absolute path) to use when launching rviz."
+            'rviz_config',
+            default_value=PathJoinSubstitution([
+                FindPackageShare('skarpulator_description'),
+                'rviz',
+                'view.rviz'
+            ]),
+            description='Rviz config file (absolute path) to use when launching rviz.'
         ),
         rsp_node,
         rviz_node,
